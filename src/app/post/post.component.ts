d53227560391e6ed313ed447d69e8914857133c9
Masayoshi <masasensei26@mail.com>
2024-04-21 11:21:42 +0700

    first commit

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrl: './post.component.css',
+})
+export class PostComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
